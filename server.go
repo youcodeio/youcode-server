53d69ed604d7cde9181dfc7fcf0870a20a195afe
PierreZ <pierre.zemb.isen@gmail.com>
2015-03-16 09:32:20 +0100

    fixing mime type for svg

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package server
 
 // Based on http://blog.extramaster.net/2014/05/creating-static-server-in-go-for-google.html
 import (
+	"mime"
 	"net/http"
 )
 
@@ -10,5 +11,6 @@ func init() {
 }
 
 func static(w http.ResponseWriter, r *http.Request) {
+	mime.AddExtensionType(".svg", "image/svg+xml")
 	http.ServeFile(w, r, "public/"+r.URL.Path)
 }
